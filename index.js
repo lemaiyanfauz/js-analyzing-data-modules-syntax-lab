e23a83f4e26b7ace7a684d26e13423ddfd8cc96e
lemaiyanfauz <lemaiyanfauz@gmail.com>
2026-04-17 10:08:52 +0300

    Implement combineUsers function in index.js
    Added combineUsers function to merge user arrays.

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
+require('datejs');
 
+function combineUsers(...args) {
+  const combinedObject = {
+    users: [],
+    merge_date: new Date().toString('M/d/yyyy')
+  };
+
+  for (let i = 0; i < args.length; i++) {
+    combinedObject.users = combinedObject.users.concat(args[i]);
+  }
+
+  return combinedObject;
+}
 
 
 module.exports = {
